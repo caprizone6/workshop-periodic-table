6a19db927018a5fe40c5e3ea9a657e586b8f2f7c
caprizone6 <caprizone6@gmail.com>
2023-02-25 14:30:21 -0600

    good solution with index and smaller lookup table

--- js/speller.js
+++ js/speller.js
@@ -18,36 +18,72 @@ async function loadPeriodicTable() {
 	}
 }
 
-function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols return array with
-	// them if so (empty array otherwise)
-	if (inputWord.length > 0) {
-		for (let el of elements) {
-			let symbol = el.symbol.toLowerCase()
-			if (symbol.length <= inputWord.length) {
-				// did the symbol match the first 
-				// one of two characters in `inputword`
-				if (inputWord.slice(0, symbol.length) == symbol) {
-					// still have characters left
-					if (inputWord.length > symbol.length) {
-						let res = check(inputWord.slice(symbol.length))
+function findCandidates(inputWord) {
+	let oneLetterSymbols = []
+	let towLetterSymbols = []
 
-						// matched successfully?
-						if (res.length > 0) {
-							return [symbol, ...res]
-						}
-					}
-					else {
-						 return [symbol]
-					}
+	for (let i = 0; i < inputWord.length; i++) {
+		// collect one-letter candidates
+		if (inputWord[i] in symbols &&
+			!oneLetterSymbols.includes(inputWord[i])) {
+				oneLetterSymbols.push(inputWord[i])
+			}
 
+		// collect two-letter candidates
+		if (i <= (inputWord.length - 2)) {
+			let two = inputWord.slice(i, i+2)
+			if (two in symbols && !towLetterSymbols.includes(two)){
+				towLetterSymbols.push(two)
+			}
+		}
+	}
+	return [ ...towLetterSymbols, ...oneLetterSymbols]
+}
+
+function spellWord(candidates,charsLeft) {
+	if (charsLeft.length == 0) {
+		return []
+	}
+	else {
+		// check for two letter symbols first
+		if (charsLeft.length >= 2) {
+			let two = charsLeft.slice(0,2)
+			let rest = charsLeft.slice(2)
+			if (candidates.includes(two)) {
+				if (rest != '') {
+					let result = [ two, ...spellWord(candidates,rest) ]
+					if (result.join('') == charsLeft) {
+						return result
+					}
+				}
+				else {
+					return [ two ]
+				}
+			}
+		}
+		// now check for one letter symbols
+		if (charsLeft.length >= 1) {
+			let one = charsLeft[0]
+			let rest = charsLeft.slice(1)
+			if (candidates.includes(one)) {
+				if (rest != '') {
+					let result = [ one, ...spellWord(candidates,rest) ]
+					if (result.join('') == charsLeft) {
+						return result
+					}
+				}
+				else {
+					return [ one ]
 				}
 			}
 		}
 	}
-
 	return []
+}
+
+function check(inputWord) {
+	let candidates = findCandidates(inputWord)
+	return spellWord(candidates, inputWord)
 }
 
 function lookup(elementSymbol) {
